a84d043e001cb2246ad5558bd34f789ff5658982
Nirav Joshi <joshi.nirav21@gmail.com>
2026-07-10 10:49:03 +0530

    Elasticsearch with spring boot playground application: Basic testcontainer and create index related trial test cases

---
+++ spring-boot-elasticsearch-playground/elasticcontainer-spring-boot-playgroundapp/src/test/java/com/nj/learning/elasticsearch/springboot/app/playground/sec01/IndexOperationsTest.java
@@ -0,0 +1,37 @@
+package com.nj.learning.elasticsearch.springboot.app.playground.sec01;
+
+import com.nj.learning.elasticsearch.springboot.app.playground.AbstractTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.IndexOperations;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+
+public class IndexOperationsTest extends AbstractTest {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(IndexOperationsTest.class);
+
+    @Autowired
+    private ElasticsearchOperations elasticsearchOperations;
+
+    @Test
+    public void createIndex() {
+        var indexOperations = this.elasticsearchOperations.indexOps(IndexCoordinates.of("albums"));
+        Assertions.assertTrue(indexOperations.create());
+        this.verify(indexOperations, 1, 1);
+    }
+
+    private void verify(IndexOperations indexOperations, int expectedShards, int expectedReplicas){
+        var settings =  indexOperations.getSettings();
+        log.info("settings: {}", settings);
+        log.info("mapping: {}", indexOperations.getMapping());
+
+        Assertions.assertEquals(String.valueOf(expectedShards), settings.get("index.number_of_shards"));
+        Assertions.assertEquals(String.valueOf(expectedReplicas), settings.get("index.number_of_replicas"));
+
+        // delete the index
+        Assertions.assertTrue(indexOperations.delete());
+
+    }
+}
