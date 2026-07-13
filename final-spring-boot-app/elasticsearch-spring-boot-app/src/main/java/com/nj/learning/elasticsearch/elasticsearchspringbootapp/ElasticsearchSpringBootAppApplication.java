502f9a4b9ba290ebf850663a6201ab27e57bceeb
Nirav Joshi <joshi.nirav21@gmail.com>
2026-07-13 14:10:48 +0530

    Initial commit with suggestion API related classes and artifacts for spring boot applicaiton

---
+++ final-spring-boot-app/elasticsearch-spring-boot-app/src/main/java/com/nj/learning/elasticsearch/elasticsearchspringbootapp/ElasticsearchSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.nj.learning.elasticsearch.elasticsearchspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchSpringBootAppApplication.class, args);
+    }
+
+}
